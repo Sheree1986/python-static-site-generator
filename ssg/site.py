a7920ca894a197a50e9d41dfc9ceef45d28428ac
Sheree <sheree.edmund@verizon.com>
2022-09-27 18:48:00 -0400

    fixed init with parsers information

--- ssg/site.py
+++ ssg/site.py
@@ -5,6 +5,7 @@ class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -15,6 +16,14 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+                
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
                 
