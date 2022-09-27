ccf7e72a2a0a938c25920c181c78d283066af561
Sheree <sheree.edmund@verizon.com>
2022-09-27 18:44:03 -0400

    fixing indentation error

--- ssg/site.py
+++ ssg/site.py
@@ -17,8 +17,8 @@ class Site:
                 self.create_dir(path)
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
-        
-       if parser is not None:
+                
+        if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
             print("Not implemented!")
